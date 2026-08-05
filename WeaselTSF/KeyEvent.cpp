27606d2b8a65fcd92d3843a406b33383b30d962b
RYUKOUKOU <123858678+RYUKOUKOU@users.noreply.github.com>
2026-08-05 15:00:51 +0900

    Add configurable Japanese keyboard layout

--- WeaselTSF/KeyEvent.cpp
+++ WeaselTSF/KeyEvent.cpp
@@ -1,5 +1,22 @@
-﻿#include "stdafx.h"
+#include "stdafx.h"
 #include <KeyEvent.h>
+
+namespace {
+HKL GetConfiguredKeyboardLayout() {
+  DWORD layout = 0;
+  DWORD size = sizeof(layout);
+  if (RegGetValueW(HKEY_CURRENT_USER, L"Software\\Rime\\Weasel",
+                   L"KeyboardLayout", RRF_RT_REG_DWORD, nullptr, &layout,
+                   &size) == ERROR_SUCCESS &&
+      layout == 1) {
+    static HKL japanese_layout =
+        LoadKeyboardLayoutW(L"00000411", KLF_NOTELLSHELL);
+    if (japanese_layout)
+      return japanese_layout;
+  }
+  return GetKeyboardLayout(0);
+}
+}  // namespace
 
 bool ConvertKeyEvent(UINT vkey,
                      KeyInfo kinfo,
@@ -48,7 +65,8 @@ bool ConvertKeyEvent(UINT vkey,
   memcpy(table, keyState, sizeof(table));
   table[VK_CONTROL] = 0;
   table[VK_MENU] = 0;
-  int ret = ToUnicodeEx(vkey, UINT(kinfo), table, buf, buf_len, 0, NULL);
+  int ret = ToUnicodeEx(vkey, UINT(kinfo), table, buf, buf_len, 0,
+                        GetConfiguredKeyboardLayout());
   if (ret == 1) {
     result.keycode = UINT(buf[0]);
     return true;
